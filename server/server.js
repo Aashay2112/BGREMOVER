f6ec9ebfa007c1d9540c112da5ab71d4c854cb7d
Aashay2112 <2k23.csaiml2313753@gmail.com>
2025-11-04 14:08:43 +0530

    webhooks added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 
 //App config
@@ -19,6 +20,8 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('api working')
 })
+
+app.use('/api/user',userRouter)
 
 app.listen(PORT,()=>{
     console.log('server running on PORT'+ PORT)
